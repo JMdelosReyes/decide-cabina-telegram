2afd1a65fcb47dbe76ea54e7e182c90cfe9ba91a
christian-martinez-andrades <chrmarand@alum.us.es>
2020-01-12 23:48:02 +0100

    Test heroku (test)
    Test heroku dats changed

--- test/test.py
+++ test/test.py
@@ -11,7 +11,7 @@ class TestMethods(unittest.TestCase):
         headers = {"Content-type": "application/json",
         "Accept": "text/plain"}
 
-        credentials = {"username": "administrador", "password":"administrador"}
+        credentials = {"username": "decide-moltres", "password":"egc-decide-moltres201920"}
         r = requests.post(config.API_DECIDE + "authentication/login/", credentials)
         self.assertEqual(r.status_code, 200)
 
@@ -28,7 +28,7 @@ class TestMethods(unittest.TestCase):
 
         headers = {"Authorization": "Token 7241666026815e02759fde720bb11c40d01edf21"}
         vote = {
-                "voting": 1,
+                "voting": 3,
                 "voter": 1,
                 "vote": { "a": "3", "b": "2" }
             }
@@ -38,12 +38,12 @@ class TestMethods(unittest.TestCase):
 
     def test_vote(self):
 
-        headers = {"Authorization": "Token af6d8a0e398dda94bc6039141708af60f284a3c8"}
+        headers = {"Authorization": "Token 	963edaa37004c7a0cdf5f8d464740e85cbb541d5"}
         vote = {
                 "vote": { "a": "3", "b": "2" },
                 "voting": 1,
                 "voter": 1,
-                "token": 'af6d8a0e398dda94bc6039141708af60f284a3c8'
+                "token": '	963edaa37004c7a0cdf5f8d464740e85cbb541d5'
             }
         r = requests.post(config.API_DECIDE + "store/", json=vote, headers = headers)
         self.assertEqual(r.status_code, 200)
